a827af8c513a99604f8e9f91a25b99e5bc8182a5
eddie0mohammed <me3dun@gmail.com>
2019-06-20 03:38:22 -0700

    modified

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBFUHV3vBOcPsExbyb2Utw4V0GFIrtG5q0';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3' ,
